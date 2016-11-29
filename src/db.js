31512c7d658f4008f3623216ec006cf97c3e8028
Kosta Dunn <kostacoffee@gmail.com>
2016-11-29 20:56:07 +1100

    Refactored database config into separate file.

--- src/db.js
+++ src/db.js
@@ -1,36 +1,20 @@
-import r from 'rethinkdbdash'
+import r from 'rethinkdbdash';
+import config from '../config';
 
-var config = {
-	servers: [{host: 'rethink', port: 28015}],
-	db: "SUITS",
-	user: 'suits',
-	password: 'EWa^kF#Z:@/%h`72'
-}
-
-var db = r(config);
+var db = r(config.db);
 
 export async function addMember(firstName, lastName, gender, email, access, sid, newsletter){
-	try{
-		return await db.table('Member').insert({
-			firstName: firstName,
-			lastName: lastName,
-			gender: gender,
-			email: email,
-			access: access,
-			sid: sid,
-			newsletter: newsletter
-		}).run();
-	}	
-	catch (e){
-		return null;
-	}
+	return await db.table('Member').insert({
+		firstName: firstName,
+		lastName: lastName,
+		gender: gender,
+		email: email,
+		access: access,
+		sid: sid,
+		newsletter: newsletter
+	}).run();
 }
 
 export async function getMembers() {
-	try {
-		return await db.table('Member').run();
-	}
-	catch (e) {
-		return null;
-	}
+	return await db.table('Member').run();
 }
