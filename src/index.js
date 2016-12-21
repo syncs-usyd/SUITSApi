88616d4a28b452f333ecb26ff47b478a3a0b9a25
Kosta Dunn <kostacoffee@gmail.com>
2016-12-21 17:15:22 +1100

    Added `Authorzation` as an allowed CORS header

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.use( async (ctx, next) => {
 // CORS
 app.use( async (ctx, next) => {
 	ctx.set("Access-Control-Allow-Origin", "*");
-	ctx.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	ctx.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 	await next();
 });
 
