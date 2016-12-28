7a0de458898e80c1d0d8b3834cb78f5ab74ec910
Kosta Dunn <kostacoffee@gmail.com>
2016-12-28 16:46:31 +1100

    Added CORS allowed methods

--- src/index.js
+++ src/index.js
@@ -27,6 +27,7 @@ app.use( async (ctx, next) => {
 app.use( async (ctx, next) => {
 	ctx.set("Access-Control-Allow-Origin", "*");
 	ctx.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+	ctx.set("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH, HEAD");
 	await next();
 });
 
