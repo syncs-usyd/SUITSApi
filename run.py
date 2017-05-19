34b25a39117830261ae1030e54bec4289b1a3ac1
Kosta Dunn <kostacoffee@gmail.com>
2017-05-19 19:10:47 +1000

    Added command line args to run.py

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
 #! /usr/bin/env python3
 from app import app
+from db import db
+from argparse import ArgumentParser
+parser = ArgumentParser()
+parser.add_argument('--dev', action='store_true', help='Enables debug mode on the app')
+args = parser.parse_args()
+if args.dev:
+    app.run(debug=True)
+else:
+    app.run()
 
-app.run(debug=True)
