f6af3e8935ce5154b4737fc8b4764f4bfdfc0888
Kosta Dunn <kostacoffee@gmail.com>
2017-05-30 10:56:24 +1000

    Added creation of tables on dev mode

--- run.py
+++ run.py
@@ -5,6 +5,8 @@ parser = ArgumentParser()
 parser.add_argument('--dev', action='store_true', help='Enables debug mode on the app')
 args = parser.parse_args()
 if args.dev:
+    from db import db
+    db.create_all()
     app.run(debug=True)
 else:
     app.run()
