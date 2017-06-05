df148f62b9502ed469c970cd8f563f5697c9a381
Kosta Dunn <kostacoffee@gmail.com>
2017-06-05 14:24:17 +1000

    Added username/password fields to db urls

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 from flask import Flask
+from settings import DB_USER, DB_PASS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://localhost:3306/api'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{}:{}@localhost:3306/api'.format(DB_USER, DB_PASS)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # initiaizing the plugins for the app
