0d48e99ca42db961a59c16b5891d669b85c02d4f
David Vo <auscompgeek@users.noreply.github.com>
2019-07-20 19:14:28 +1000

    Remove typescript runtime dep
    Reduces RAM usage a fair bit.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,19 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { RavenModule } from "nest-raven";
 import { ApiModule } from "./api/api.module";
 import { CoreModule } from "./core/core.module";
+import { AttendanceEntity, EventEntity, MemberEntity } from "./entities";
+
+const ormconfig = require("../ormconfig.json");
 
 @Module({
-    imports: [TypeOrmModule.forRoot(), CoreModule, ApiModule, RavenModule],
+    imports: [
+        TypeOrmModule.forRoot({
+            ...ormconfig,
+            entities: [AttendanceEntity, EventEntity, MemberEntity],
+        }),
+        CoreModule,
+        ApiModule,
+        RavenModule,
+    ],
 })
 export class ApplicationModule {}
