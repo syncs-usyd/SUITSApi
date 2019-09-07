ff118b047d50214ae6a800f36aea26a04a7eab03
David Vo <auscompgeek@users.noreply.github.com>
2019-09-07 16:29:01 +1000

    Fix systemd notify

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import * as Sentry from "@sentry/node";
 import * as express from "express";
 import { ApplicationModule } from "./app.module";
-import { notify } from "./sdnotify";
 
 // tslint:disable-next-line: no-var-requires
 const config = require("../config.json");
@@ -35,7 +34,12 @@ async function bootstrap() {
     process.on("SIGINT", () => app.close());
     process.on("SIGTERM", () => app.close());
 
-    notify("READY=1");
+    try {
+        const sd_notify = require("sd_notify_promise").sd_notify;
+        await sd_notify("READY=1");
+    } catch (ex) {
+        // ignore if sd_notify_promise isn't installed or errors
+    }
 }
 
 bootstrap();
